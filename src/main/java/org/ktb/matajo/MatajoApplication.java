eb661d75f15061b80609b02b36a5ccc4917ea35e
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-12 18:27:05 +0900

    feat: 스프링 부트 프로젝트 초기 설정

---
+++ src/main/java/org/ktb/matajo/MatajoApplication.java
@@ -0,0 +1,13 @@
+package org.ktb.matajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatajoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatajoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ktb/matajo/MatajoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ktb.matajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatajoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
