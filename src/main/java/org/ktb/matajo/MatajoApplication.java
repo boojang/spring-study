c85051c45d07a134d4865df7950f2a0c35a93ec5
ghkddlscks19 <ghkddlscks19@naver.com>
2025-03-13 13:16:39 +0900

    style: 메인 함수 공백 및 줄바꿈 수정

--- src/main/java/org/ktb/matajo/MatajoApplication.java
+++ src/main/java/org/ktb/matajo/MatajoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MatajoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MatajoApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MatajoApplication.class, args);
+  }
 
 }
